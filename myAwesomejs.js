9aabd0f3bbea4d7c6717709827a99edba298864f
Johnny Goth <Jgoth23@gmail.com>
2021-01-28 14:56:48 -0600

    did some stuff

--- myAwesomejs.js
+++ myAwesomejs.js
@@ -1,1 +1,33 @@
 console.log('js');
+$(document).ready(onReady);
+
+function onReady() {
+  console.log('So ready');
+
+  const h1 = $('h1');
+  console.log(h1);
+
+  const h2 = $('h2');
+  h2.css('background-color', 'blue');
+
+  $('h2').addClass('froggy');
+
+  const h1Text = $('h1').text();
+  console.log('h1Text is', h1Text);
+
+  const louderText = h1Text.toUpperCase() + '!!!';
+  $('h1').text(louderText);
+  $('#submitbutton').on('click', whenIRide);
+}
+
+function whenIRide() {
+  console.log('clicked the button');
+  let brand = $('#brand').val();
+  console.log('brand is', brand);
+  $('#Bikes').append(`
+    <li>
+      brand is: ${brand}
+      <button class="deleteButton">Delete Me</button>
+    </li>
+  `);
+}
